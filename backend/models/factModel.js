c23ba6a65b19f755f20b0f67461bcf622b9f9c85
jjenska <jenni.erkkila@myy.haaga-helia.fi>
2023-04-27 18:32:13 +0300

    schema field add

--- backend/models/factModel.js
+++ backend/models/factModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const factSchema = mongoose.Schema({
   fact: { type: String, required: true },
   length: { type: Number },
+  breed: { type: String },
 });
 
 module.exports = mongoose.model("Fact", factSchema);

c2800d2de53b8dbb2de5eaaeb0afe17eb135940c
jjenska <jenni.erkkila@myy.haaga-helia.fi>
2023-05-03 16:28:32 +0300

    schema fix

--- backend/models/factModel.js
+++ backend/models/factModel.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const factSchema = mongoose.Schema({
   fact: { type: String, required: true },
   length: { type: Number },
-  breed: { type: String },
 });
 
 module.exports = mongoose.model("Fact", factSchema);
